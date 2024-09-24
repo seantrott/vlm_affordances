74506415e2c682fd9db00fe3ff62237e3deb4b04
Sean <trott.sean@gmail.com>
2024-09-24 10:09:20 -0700

    Updated with data, etc.

---
+++ src/main_study/run_gpt.py
@@ -0,0 +1,123 @@
+"""Run GPT models."""
+
+from openai import OpenAI
+import openai
+import pandas as pd
+import os
+import base64
+from tqdm import tqdm
+
+client = OpenAI()
+
+
+# Function to encode the image
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+# Function to iterate and encode images in a folder
+def encode_images_in_folder(folder_path):
+    encoded_images = {}
+    for image_name in os.listdir(folder_path):
+        image_path = os.path.join(folder_path, image_name)
+        if os.path.isfile(image_path):
+            encoded_images[image_name] = encode_image(image_path)
+    return encoded_images
+
+# Function to generate URL for image
+def generate_data_url(base64_image, image_name):
+    if image_name.lower().endswith('.png'):
+        mime_type = 'image/png'
+    else:
+        mime_type = 'image/jpeg'
+    
+    return f"data:{mime_type};base64,{base64_image}"
+
+# Call to GPT4
+import backoff  # for exponential backoff
+@backoff.on_exception(backoff.expo, openai.RateLimitError)
+def gpt4_vision_call(prompt, base64_image, image_name, model="gpt-4-turbo"):
+    data_url = generate_data_url(base64_image, image_name)
+    
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "system",
+             "content": """In this task, you will read short passages and look at an image of an object.
+Please rate how sensible it would be to take the action described in the last sentence using the object in
+the image in the context of the whole passage. The scale goes from 1 (virtual nonsense) to 7 (completely sensible).
+Be sure to read the sentences carefully. Please respond only with a number between 1 and 7.
+"""},
+
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": prompt},
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": data_url,
+                        },
+                    },
+                ],
+            }
+        ],
+        max_tokens = 3,
+        temperature = 0
+    )
+    return response.choices[0].message.content
+
+
+def main(version, model_name="gpt-4-turbo"):
+
+    ### Set up paths
+    csv_path = "data/stimuli/{x}/affordance_{y}.csv".format(x = version, y= version)
+    img_folder = "data/stimuli/{x}/images".format(x = version)
+
+    ### Load data
+    df = pd.read_csv(csv_path)
+    ### Encode images
+    encoded_images_dict = encode_images_in_folder(img_folder)
+
+    ### Run through GPT-4
+    results = []
+    for _, row in tqdm(df.iterrows(), total=df.shape[0]):
+        text = row['condition']
+        
+        ### Each version/condition
+        for image_type in ['afforded_image', 'non-afforded_image', 'related_image']:
+            ### Get corresponding image for this condition
+            image_name = row[image_type]
+            if image_name in encoded_images_dict:
+                base64_image = encoded_images_dict[image_name]
+                result = gpt4_vision_call(text, base64_image, image_name)
+            else:
+                result = "Image encoding not found"
+
+            results.append({
+                'text': text,
+                'response': result,
+                'condition': image_type,
+                'img_name': image_name,
+                'response_type': 'generation',
+                'group_id': row['group_id'],
+                'model_name': model_name,
+                'version': version,
+                'prompt_type': row['prompt_type']
+                })
+    
+
+    df_results = pd.DataFrame(results)
+
+    # Save to .csv
+    SAVE_PATH = "data/processed/models/closed_models/{model}_{version}_affordances.csv".format(model = model_name, version = version)
+    print("Saving to " + SAVE_PATH)
+    df_results.to_csv(SAVE_PATH, index = False)
+
+
+MODELS = ['gpt-4o', 'gpt-4-turbo']
+
+for mpath in MODELS:
+    print(mpath)
+    for version in ['natural', 'synthetic']:
+        main(version, mpath)
---
+++ src/main_study/run_hf_models.py
@@ -0,0 +1,237 @@
+"""Run HF models on either natural or synthetic affordance images."""
+
+import pandas as pd
+import os.path as op
+import seaborn as sns
+import numpy as np
+from scipy.spatial.distance import cosine
+
+from tqdm import tqdm
+
+from transformers import CLIPProcessor, CLIPModel, FlavaProcessor, FlavaModel, BlipProcessor, BlipModel, AlignProcessor, AlignModel
+from transformers import ViltForImageAndTextRetrieval, ViltProcessor, ViltConfig, BridgeTowerForImageAndTextRetrieval, BridgeTowerProcessor
+import torch
+from PIL import Image
+from torch.nn.functional import cosine_similarity
+import torch.nn.functional as F
+
+
+
+
+
+
+### Possible models
+MODELS = {
+    'clip-vit-base-patch32': ['openai/clip-vit-base-patch32', CLIPModel, CLIPProcessor],
+    'clip-vit-large-patch14': ['openai/clip-vit-large-patch14', CLIPModel, CLIPProcessor],
+    'flava-full': ['facebook/flava-full', FlavaModel, FlavaProcessor],
+    'align-base': ['kakaobrain/align-base', AlignModel, AlignProcessor],
+    'clip-huge-14': ['laion/CLIP-ViT-H-14-laion2B-s32B-b79K', CLIPModel, CLIPProcessor],
+    'clip-giant': ['laion/CLIP-ViT-g-14-laion2B-s12B-b42K', CLIPModel, CLIPProcessor],
+    'clip-big-giant': ['laion/CLIP-ViT-bigG-14-laion2B-39B-b160k', CLIPModel, CLIPProcessor],
+    'vilt-coco': ['dandelin/vilt-b32-finetuned-coco', ViltForImageAndTextRetrieval, ViltProcessor],
+    'vilt-f30k': ['dandelin/vilt-b32-finetuned-flickr30k', ViltForImageAndTextRetrieval, ViltProcessor],
+    'bridgetower': ['BridgeTower/bridgetower-large-itm-mlm-itc', BridgeTowerForImageAndTextRetrieval, BridgeTowerProcessor]
+    ### TODO: ViLT? (Would need to use logits instead)? Bridgetower?
+    ##### Issue with VILT is context window isn't long enough?
+    ### TODO: BLIP for image-text-matching instead? There are issues with BlipModel class...
+    ### TODO: output_hidden_states for probing?
+    ### Details on Flava: "For example, FLAVA... (from Zhang et al., 2024)"
+    }
+
+
+
+def count_parameters(model):
+    """credit: https://stackoverflow.com/questions/49201236/check-the-total-number-of-parameters-in-a-pytorch-model"""
+    
+    total_params = 0
+    for name, parameter in model.named_parameters():
+        
+        # if the param is not trainable, skip it
+        if not parameter.requires_grad:
+            continue
+        
+        # otherwise, count it towards your number of params
+        params = parameter.numel()
+        total_params += params
+    print(f"Total Trainable Params: {total_params}")
+    
+    return total_params
+    
+
+
+class HFModelRunner(object):
+
+
+
+    def __init__(self, model_name, version):
+        self.model_name = model_name
+        self.model_path = MODELS[model_name][0]
+        self.version = version 
+
+        self.load_model(model_name)
+        self.load_dataset(version)
+
+    def load_model(self, model_name):
+
+        classes = (MODELS[model_name][1], MODELS[model_name][2])
+
+        self.processor = classes[1].from_pretrained(self.model_path)
+        self.model = classes[0].from_pretrained(self.model_path)
+
+        self.num_params = count_parameters(self.model)
+
+
+    def load_dataset(self, version = "natural"):
+        path = "data/stimuli/{x}/affordance_{y}.csv".format(x = version, y = version)
+        self.df = pd.read_csv(path)
+
+
+    def compare_inputs(self, text, image):
+
+        if self.model_name in ['clip-vit-base-patch32', 'clip-vit-large-patch14', 
+                              'blip-image-captioning-base', 'blip-image-captioning-large',
+                              'align-base', 'clip-huge-14', 'clip-giant', 'clip-big-giant']:
+
+            ## Encode text
+            text_input = self.processor(text=text, return_tensors="pt", padding=True)
+
+            ## Encode image
+            image_input = self.processor(images=image, return_tensors="pt")
+
+            # Get the image and text representations
+            with torch.no_grad():
+                image_features = self.model.get_image_features(**image_input)
+                text_features = self.model.get_text_features(**text_input)
+
+            # Normalize the features
+            image_features = image_features / image_features.norm(dim=-1, keepdim=True)
+            text_features = text_features / text_features.norm(dim=-1, keepdim=True)
+
+            return cosine_similarity(image_features, text_features).item()
+
+
+        elif self.model_name in ['flava-full']:
+
+            ## Encode text
+            text_input = self.processor(text=text, return_tensors="pt", padding=True)
+
+            ## Encode image
+            image_input = self.processor(images=image, return_tensors="pt")
+
+            # Get the image and text representations
+            with torch.no_grad():
+                image_features = self.model.get_image_features(**image_input)
+                text_features = self.model.get_text_features(**text_input)
+
+            ### Get CLS embedding for FLAVA
+            image_cls_embedding = image_features[:, 0, :]  # [batch_size, embed_dim]
+            text_cls_embedding = text_features[:, 0, :]    # [batch_size, embed_dim]
+            
+            # Normalize the features
+            image_cls_embedding = F.normalize(image_cls_embedding, dim=-1)
+            text_cls_embedding = F.normalize(text_cls_embedding, dim=-1)
+
+            return cosine_similarity(image_cls_embedding, text_cls_embedding).item()
+
+
+        elif self.model_name in ['vilt-coco', 'vilt-f30k']:
+
+            # Define and preprocess the text
+            ### TODO: Requires truncation in some cases
+            encoding = self.processor(image, text, return_tensors="pt", max_length=40, truncation=True)
+    
+            # Run through model
+            with torch.no_grad():
+                outputs = self.model(**encoding)
+    
+            # Return logits
+            logits = outputs.logits[0, :].item()
+
+            return logits
+
+        elif self.model_name in ['bridgetower']:
+
+            # Define and preprocess the text
+            ### TODO: Requires truncation in some cases
+            encoding = self.processor(image, text, return_tensors="pt", max_length=40, truncation=True)
+    
+            # Run through model
+            with torch.no_grad():
+                outputs = self.model(**encoding)
+    
+            # Return logits
+            ### [0, 1] is logit for probability that image/text match
+            logits = outputs.logits[0, 1].item()
+
+            return logits
+
+    def run_model(self):
+
+        ### Track results
+        results = []
+
+        for index, row in tqdm(self.df.iterrows(), total=self.df.shape[0]):
+            text = row['condition']
+            for cond in ['afforded_image', 'non-afforded_image', 'related_image']:
+
+                ## Get image path
+                img_name = row[cond]
+                path = "data/stimuli/{x}/images/".format(x = self.version)
+                img_path = op.join(path, img_name)
+
+                # Load and preprocess the image
+                image = Image.open(img_path).convert("RGB")
+
+                # Get features
+                response = self.compare_inputs(text, image)
+
+                # Response type
+                response_type = 'logits' if self.model_name in ['vilt-coco', 'bridgetower'] else 'cosine_similarity'
+
+                # track data
+                results.append({
+                    'text': text,
+                    'condition': cond,
+                    'img_name': img_name,
+                    'group_id': row['group_id'],
+                    'prompt_type': row['prompt_type'],
+                    'response_type': response_type,
+                    'response': response,
+                    'model_name': self.model_name,
+                    'version': self.version,
+                    'num_params': self.num_params
+                })
+
+        # Turn results into DataFrame
+        self.df_results = pd.DataFrame(results)
+        
+        # Save to .csv
+        SAVE_PATH = "data/processed/models/hf_models/{model}_{version}_affordances.csv".format(model = self.model_name, version = self.version)
+        print("Saving to " + SAVE_PATH)
+        self.df_results.to_csv(SAVE_PATH, index = False)
+
+
+
+### TODO: BLIP weights not initialized?
+
+models_to_run = [#'align-base',
+                # 'clip-vit-base-patch32',
+                # 'clip-vit-large-patch14',
+                # 'flava-full',
+                # 'clip-huge-14',
+                # 'clip-giant',
+                # 'clip-big-giant',
+                # 'vilt-coco',
+                'vilt-f30k',
+                # 'bridgetower'
+                ]
+for model_name in models_to_run:
+    print(model_name)
+    for version in ['natural', 'synthetic']:
+    
+    # for version in ['synthetic']:
+        hf = HFModelRunner(model_name = model_name, 
+                           version = version)
+        hf.run_model()
+
